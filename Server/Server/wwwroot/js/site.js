7299f1c9d2df0f4fec172e2b6da6a73c477dfba4
Joe <frapaquad@gmail.com>
2018-03-11 11:36:58 +0100

    Refactor load indication code into site.js

--- Server/Server/wwwroot/js/site.js
+++ Server/Server/wwwroot/js/site.js
@@ -1,1 +1,20 @@
 ﻿// Write your JavaScript code.
+var loadState = 0;
+function loadPush() {
+    loadState++;
+    loadUpdateViews();
+}
+function loadPop() {
+    loadState--;
+    loadUpdateViews();
+}
+function loadUpdateViews() {
+    if(loadState > 0) {
+        $("#busyIndicator").show();
+        $("#scaffolding").hide();
+    }
+    else {
+        $("#busyIndicator").hide();
+        $("#scaffolding").show();
+    }
+}
