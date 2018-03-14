5ba8182c62a728f21396d568412d2313c14d0361
Joe Cooper <frapaquad@gmail.com>
2018-03-14 12:10:34 +0100

    Reimplemented history graph and contributor list in javascript using API endpoints

--- Server/Server/Controllers/DocumentController.cs
+++ Server/Server/Controllers/DocumentController.cs
@@ -112,6 +112,15 @@ namespace Server.Controllers
 			return View("Document", new IdentifierViewModel(id));
 		}
 
+        [HttpGet("{id}/history")]
+		[ResponseCache(CacheProfileName = CacheProfileNames.Default)]
+		public IActionResult GetHistory(string id)
+		{
+			if (id.FalsifyAsIdentifier())
+				return BadRequest();
+			return View("History", new IdentifierViewModel(id));
+		}
+
 		[HttpGet("{id}/indexable")]
 		[ResponseCache(CacheProfileName = CacheProfileNames.SemiImmutable)]
 		public async Task<IActionResult> GetDocumentForIndexing(string id)
@@ -123,128 +132,6 @@ namespace Server.Controllers
 			var metadata = await databaseService.GetDocumentMetadataAsync(idMD5);
 			var body = await databaseService.GetDocumentBodyAsync(idMD5);
 			return View("DocumentForIndexing", new DocumentViewModel(body, metadata.Title));
-		}
-
-		async Task<IEnumerable<DocumentListingViewModel>> GetDocumentListingsAsync(NpgsqlConnection connection, IEnumerable<MD5Sum> keys)
-		{
-			var boxedKeys = keys.Select(m => m.ToGuid()).ToArray();
-
-			using (var cmd = new NpgsqlCommand())
-			{
-				cmd.Connection = connection;
-				cmd.CommandText = "SELECT id, title, authorId, timestamp FROM document WHERE ARRAY[id] && @closedSet;";
-				cmd.Parameters.AddWithValue("@closedSet", boxedKeys);
-
-				using (var reader = await cmd.ExecuteReaderAsync())
-				{
-					var documentsBuilder = ImmutableArray.CreateBuilder<DocumentListingViewModel>();
-
-					while (await reader.ReadAsync())
-					{
-						documentsBuilder.Add(
-							new DocumentListingViewModel(
-								new MD5Sum(reader.GetGuid(0).ToByteArray()),
-								reader.GetString(1),
-								reader.GetGuid(2),
-								reader.GetDateTime(3)
-							)
-						);
-					}
-
-					return documentsBuilder;
-				}
-			}
-		}
-
-		async Task<IEnumerable<AccountListingViewModel>> GetAccountListingsAsync(NpgsqlConnection connection, IEnumerable<Guid> keys)
-		{
-			using (var cmd = new NpgsqlCommand())
-			{
-				var accountListingsBuilder = ImmutableArray.CreateBuilder<AccountListingViewModel>();
-
-				cmd.Connection = connection;
-				cmd.CommandText = "SELECT id, displayName, email FROM account WHERE ARRAY[id] && @authorIds;";
-				cmd.Parameters.AddWithValue("@authorIds", keys.ToArray());
-
-				using (var reader = await cmd.ExecuteReaderAsync())
-				{
-					while (await reader.ReadAsync())
-					{
-						var accountId = reader.GetGuid(0);
-						var displayName = reader.GetString(1);
-						var email = reader.GetString(2);
-						var gravatarHash = email.ToGravatarHash();
-						accountListingsBuilder.Add(new AccountListingViewModel(accountId, displayName, gravatarHash));
-					}
-				}
-
-				return accountListingsBuilder;
-			}
-		}
-
-		IEnumerable<DocumentListingViewModel> EnrichDocumentListings(IEnumerable<DocumentListingViewModel> documentListings,
-		                                                             IEnumerable<AccountListingViewModel> accountListings)
-		{
-			var authorNames = ImmutableDictionary.CreateRange(accountListings.Select(a => new KeyValuePair<Guid, string>(a.Id, a.DisplayName)));
-			return documentListings.Select(d => d.WithAuthorDisplayName(authorNames[d.AuthorId]));
-		}
-
-		[HttpGet("{id}/history")]
-		public async Task<IActionResult> GetHistory(string id)
-		{
-			if (id.FalsifyAsIdentifier())
-				return BadRequest();
-			var idInBinary = WebEncoders.Base64UrlDecode(id);
-			var idInMD5Sum = new MD5Sum(idInBinary);
-			var idBoxedInGuidForDatabase = new Guid(idInBinary);
-
-			IEnumerable<DocumentListingViewModel> documentsInFamily;
-			IEnumerable<AccountListingViewModel> accountListings;
-			IEnumerable<Relation> relations;
-
-			using (var conn = new NpgsqlConnection(databaseConfiguration.ConnectionString))
-			{
-				await conn.OpenAsync();
-
-				relations = await databaseService.GetFamilyAsync(idInMD5Sum);
-
-				{
-					var allIdsBuilder = ImmutableHashSet.CreateBuilder<MD5Sum>();
-					allIdsBuilder.UnionWith(relations.Select(r => r.AntecedentId));
-					allIdsBuilder.UnionWith(relations.Select(r => r.DescendantId));
-					allIdsBuilder.Add(idInMD5Sum);
-					documentsInFamily = await GetDocumentListingsAsync(conn, allIdsBuilder);
-				}
-
-				accountListings = await GetAccountListingsAsync(conn, documentsInFamily.Select(d => d.AuthorId));
-			}
-
-			IImmutableSet<Guid> contributorIds;
-			{
-				var closedSet = ImmutableHashSet.CreateBuilder<MD5Sum>();
-				var openSet = ImmutableHashSet.CreateBuilder<MD5Sum>();
-
-				openSet.Add(new MD5Sum(idInBinary));
-
-				do
-				{
-					var antecedentIds = ImmutableArray.CreateRange(relations.Where(r => openSet.Contains(r.DescendantId)).Select(r => r.AntecedentId));
-					closedSet.UnionWith(openSet);
-					openSet.UnionWith(antecedentIds);
-					openSet.ExceptWith(closedSet);
-				} while (openSet.Count > 0);
-
-				var documentsInHistory = documentsInFamily.Where(d => closedSet.Contains(d.Id));
-				contributorIds = ImmutableHashSet.CreateRange(documentsInHistory.Select(d => d.AuthorId));
-			}
-
-			documentsInFamily = EnrichDocumentListings(documentsInFamily, accountListings);
-
-			var contributorListings = accountListings.Where(c => contributorIds.Contains(c.Id));
-
-			var subject = documentsInFamily.Single(d => d.Id.Equals(idInBinary));
-
-			return View("History", new HistoryViewModel(subject, relations, documentsInFamily, accountListings));
 		}
     }
 }
--- Server/Server/wwwroot/js/site.js
+++ Server/Server/wwwroot/js/site.js
@@ -1,25 +1,42 @@
-﻿// Write your JavaScript code.
-var loadState = 0;
-function loadPush() {
-    loadState++;
-    loadUpdateViews();
+﻿Array.prototype.firstOrDefault = function(func){
+    return this.filter(func)[0] || null;
+};
+class LoadManager {
+	constructor(onStart, onComplete) {
+		this.onStart = onStart;
+		this.onComplete = onComplete;
+		this.state = 0;
+	}
+	push() {
+		this.state = this.state + 1;
+		if(this.state == 1) {
+			this.onStart();
+		}
+	}
+	pop() {
+		this.state = this.state - 1;
+		if(this.state == 0) {
+			this.onComplete();
+		}
+	}
 }
-function loadPop(onComplete = function () {}) {
-    loadState--;
-    if(loadState == 0) {
-	    onComplete();
-    }
-    loadUpdateViews();
-}
-function loadUpdateViews() {
-    if(loadState > 0) {
+const mainPageLoad = new LoadManager(
+	function () {
         $("#busyIndicator").show();
         $("#scaffolding").hide();
-    }
-    else {
+	},
+	function () {
         $("#busyIndicator").hide();
         $("#scaffolding").show();
-    }
+	});
+function loadPush() {
+	mainPageLoad.push();
+}
+function loadPop(onComplete = function () {}) {
+	mainPageLoad.pop();
+	if(mainPageLoad.state == 0) {
+	    onComplete();
+	}
 }
 function escapeHtml(text) {
 	var map = {
