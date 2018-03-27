828d4a007286f467b17bfa655df06a5f046b8700
Joe Cooper <frapaquad@gmail.com>
2018-03-27 09:35:49 +0200

    Added method for correctly deriving the client's IP address from the request.

---
+++ Server/Server/Utilities/RemoteAddress.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.Net;
+using Microsoft.AspNetCore.Http;
+
+namespace Server.Utilities
+{
+	public static class RemoteAddress
+	{
+		public static IPAddress GetRemoteAddress(this HttpContext context)
+		{
+			IPAddress remoteAddress;
+			const string ForwardedForHeaderKey = "X-Forwarded-For";
+			if(context.Request.Headers.ContainsKey(ForwardedForHeaderKey) && IPAddress.TryParse(context.Request.Headers[ForwardedForHeaderKey].Last(), out remoteAddress))
+			{
+				//This block is empty. This could be organized better but organized like so, the compiler can correctly evaluate
+				//that remoteAddress is assigned to.
+			}
+			else
+			{
+				remoteAddress = context.Connection.RemoteIpAddress;
+			}
+			return remoteAddress;
+		}
+	}
+}
